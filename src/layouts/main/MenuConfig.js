e97ba48f48bde05d0efd770048acc43a6ce36a63
myhency <hency.yeo@gmail.com>
2021-12-08 01:08:24 +0900

    NFT-39 사용자 지갑으로 부터 서명을 받는것 까지 구현함

--- src/layouts/main/MenuConfig.js
+++ src/layouts/main/MenuConfig.js
@@ -4,7 +4,7 @@ import fileFill from '@iconify/icons-eva/file-fill';
 import roundGrain from '@iconify/icons-ic/round-grain';
 import bookOpenFill from '@iconify/icons-eva/book-open-fill';
 // routes
-import { PATH_AUTH, PATH_DOCS, PATH_PAGE, PATH_DASHBOARD } from '../../routes/paths';
+import { PATH_AUTH, PATH_DOCS, PATH_PAGE, PATH_DASHBOARD, PATH_ASSET } from '../../routes/paths';
 
 // ----------------------------------------------------------------------
 
@@ -22,7 +22,7 @@ const menuConfig = [
   {
     title: 'Create',
     icon: <Icon icon={homeFill} {...ICON_SIZE} />,
-    path: PATH_PAGE.create
+    path: PATH_ASSET.create
   },
   {
     title: 'Profile',
--- src/pages/asset/AssetCreate.js
+++ src/pages/asset/AssetCreate.js
@@ -3,6 +3,9 @@ import { paramCase } from 'change-case';
 import { useParams, useLocation } from 'react-router-dom';
 // material
 import { Container } from '@mui/material';
+// for Wallet
+import Web3Modal from 'web3modal';
+import { ethers, providers } from 'ethers';
 // redux
 import { useDispatch, useSelector } from '../../redux/store';
 import { getUserList } from '../../redux/slices/user';
@@ -25,6 +28,23 @@ export default function AssetCreate() {
   const { userList } = useSelector((state) => state.user);
   const isEdit = pathname.includes('edit');
   const currentUser = userList.find((user) => paramCase(user.name) === name);
+
+  useEffect(() => {
+    async function web3Tasks() {
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+
+      const message = 'My first test message';
+      const result = await signer.signMessage(message);
+      console.log(result);
+      const verify = ethers.utils.verifyMessage(message, result);
+      console.log(verify);
+    }
+
+    web3Tasks();
+  }, []);
 
   useEffect(() => {
     dispatch(getUserList());
